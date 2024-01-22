193da989d1d0db273dd5c6d9bab599725221ffd9
Vrushabh Lewade <vrushabhlewade@gmail.com>
2024-01-23 02:40:37 +0530

    First Commit

---
+++ app/src/main/java/com/example/easyfood/activities/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.easyfood.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.example.easyfood.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.btm_nav)
+        setupWithNavController(bottomNavigationView, navController)
+    }
+}
---
+++ app/src/main/java/com/example/easyfood/fragments/HomeFragment.kt
@@ -0,0 +1,57 @@
+package com.example.easyfood.fragments
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
+import com.example.easyfood.activities.MealActivity
+import com.example.easyfood.databinding.FragmentHomeBinding
+import com.example.easyfood.videoModel.HomeViewModel
+
+class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
+    private lateinit var homeMvvm: HomeViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        homeMvvm = ViewModelProvider(this)[HomeViewModel::class.java]
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        homeMvvm.getRandomMeal()
+        observerRandomMeal()
+        onRandomMealClick()
+    }
+
+    private fun onRandomMealClick() {
+        binding.randomMealCard.setOnClickListener {
+            val intent = Intent(activity, MealActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun observerRandomMeal() {
+        homeMvvm.observeRandomMealLiveData().observe(
+            viewLifecycleOwner
+        ) { value ->
+            Glide.with(this@HomeFragment)
+                .load(value!!.strMealThumb)
+                .into(binding.imgRandomMeal)
+        }
+    }
+}
